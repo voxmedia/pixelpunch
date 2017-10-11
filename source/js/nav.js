b5f7e57490a018d62d3884a185fb7044c1a68771
Matt Sullivan <matt.sullivan@sbnation.com>
2017-10-11 12:38:28 -0400

    Small tweak

--- source/js/nav.js
+++ source/js/nav.js
@@ -1,36 +1,27 @@
 $(document).ready(function(){
 
-    /**
-     * This part causes smooth scrolling using scrollto.js
-     * We target all a tags inside the nav, and apply the scrollto.js to it.
-     */
     $("nav a").click(function(evn){
         evn.preventDefault();
         $('html,body').scrollTo(this.hash, this.hash);
     });
 
-    /**
-     * This part handles the highlighting functionality.
-     * We use the scroll functionality again, some array creation and
-     * manipulation, class adding and class removing, and conditional testing
-     */
-    var aChildren = $("nav li").children(); // find the a children of the list items
-    var aArray = []; // create the empty aArray
+    var aChildren = $("nav li").children();
+    var aArray = [];
     for (var i=0; i < aChildren.length; i++) {
         var aChild = aChildren[i];
         var ahref = $(aChild).attr('href');
         aArray.push(ahref);
-    } // this for loop fills the aArray with attribute href values
+    }
 
     $(window).scroll(function(){
-        var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
-        var windowHeight = $(window).height(); // get the height of the window
+        var windowPos = $(window).scrollTop();
+        var windowHeight = $(window).height();
         var docHeight = $(document).height();
 
         for (var i=0; i < aArray.length; i++) {
             var theID = aArray[i];
-            var divPos = $(theID).offset().top; // get the offset of the div from the top of page
-            var divHeight = $(theID).height(); // get the height of the div in question
+            var divPos = $(theID).offset().top;
+            var divHeight = $(theID).height();
             if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
                 $("a[href='" + theID + "']").addClass("nav-active");
             } else {
